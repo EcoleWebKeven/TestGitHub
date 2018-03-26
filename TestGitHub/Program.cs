acfd396c34f31571289ca303f82d166f7454eb9e
Kéven Thériault <keven.theriault@cegeprdl.ca>
2018-03-26 08:24:20 -0400

    Ajout de la sortie

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -1,10 +1,13 @@
 ﻿
+using System;
+
 namespace TestGitHub
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("GitHub");
         }
     }
 }
